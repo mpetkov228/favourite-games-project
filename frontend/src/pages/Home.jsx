b1d7eb3b9c52a0951f89b04edfaf9ff66455cd45
MihailPetkov96 <mpetkov228@gmail.com>
2025-11-06 16:16:18 +0200

    Add a search bar to Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,38 @@
+import { useState } from 'react';
 import GameCard from '../components/GameCard';
 import '../css/Home.css';
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState('');
+
     const games = [
         { url: '', title: 'Far Cry', releaseYear: 2004 },
         { url: '', title: 'Super Mario', releaseYear: 1985 },
-        { url: '', title: 'Grand Theft Auto III', releaseYear: 2001 }
+        { url: '', title: 'Grand Theft Auto III', releaseYear: 2001 },
+        { url: '', title: 'Grand Theft Auto III', releaseYear: 2001 },
     ];
 
+    const handleSearch = (e) => {
+        e.preventDefault();
+        alert(searchQuery);
+        setSearchQuery('');
+    }
+
     return (
-        <div className='games-grid'>
-            {games.map(game => <GameCard game={game} />)}
+        <div className='home'>
+            <form onSubmit={handleSearch} className='search-form'>
+                <input 
+                    type="text"
+                    placeholder="Search games..."
+                    className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
+            <div className='games-grid'>
+                {games.map(game => <GameCard game={game} />)}
+            </div>
         </div>
     );
 }
