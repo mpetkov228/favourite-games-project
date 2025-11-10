ccd4795b8e2df6d2d9561e3e5b1c85092708e91f
MihailPetkov96 <mpetkov228@gmail.com>
2025-11-10 21:21:11 +0200

    Create Library context for adding games to your library

---
+++ frontend/src/contexts/LibraryContext.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect, createContext, useContext } from "react";
+
+const LibraryContext = createContext();
+
+export const useLibraryContext = () => useContext(LibraryContext);
+
+export const LibraryProvider = ({ children }) => {
+    const [library, setLibrary] = useState([]);
+
+    useEffect(() => {
+        const storedLib = localStorage.getItem('library');
+
+        if (storedLib) setLibrary(JSON.parse(storedLib));
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem('library', JSON.stringify(library));
+    }, [library]);
+
+    const addToLibrary = ({ game }) => {
+        setLibrary(state => [...state, game]);
+    };
+
+    const isInLibrary = ({ gameId }) => {
+        return library.some(game => game.id === gameId);
+    };
+
+    const value = {
+        library,
+        addToLibrary,
+        isInLibrary
+    };
+
+    return (
+        <LibraryContext.Provider value={value}>
+            { children }
+        </LibraryContext.Provider>
+    );
+};
