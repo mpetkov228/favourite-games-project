6935851940bed9c3fbcbbc734c713701f2e43681
MihailPetkov96 <mpetkov228@gmail.com>
2025-11-17 20:53:28 +0200

    Connect to db

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hi');
+const users = [
+    { id: 1, name: 'Name1' },
+    { id: 2, name: 'Name2' }
+];
+
+app.get('/users', (req, res) => {
+    res.json(users);
 });
 
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-});
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('connected to db');
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`)
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
